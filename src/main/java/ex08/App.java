28ffcf1e30834ddfcfd3cb537f56fbfccf4b6dbd
savosburn <savosburn18@tampabay.rr.com>
2021-06-06 21:32:41 -0400

    challenge 2 and 3

--- src/main/java/ex08/App.java
+++ src/main/java/ex08/App.java
@@ -13,37 +13,69 @@ public class App {
     public static void main(String[] args) {
         App myApp = new App();
 
+        // Input
         int numPeople = myApp.numOfPeople();
         int numPizzas = myApp.numOfPizzas();
         int numSlices = myApp.slicesPerPizza();
 
+        // Calculations
         int totalSlices = numPizzas * numSlices;
         int pizzaPerPerson = totalSlices / numPeople;
         int leftoverPizza = totalSlices % numPeople;
 
+        // Output
         String generateOutput = myApp.stringConcat(numPeople, numPizzas, totalSlices, pizzaPerPerson, leftoverPizza);
         myApp.output(generateOutput);
     }
 
     public int numOfPeople() {
         System.out.print("How many people? ");
+
+        // User can only enter number
+        while (!in.hasNextInt()) {
+            System.out.print("Invalid amount of people. Enter again: ");
+            in.next();
+        }
+
         return in.nextInt();
     }
 
     public int numOfPizzas() {
         System.out.print("How many pizzas do you have? ");
+
+        // User can only enter number
+        while (!in.hasNextInt()) {
+            System.out.print("Invalid number of pizzas. Enter again: ");
+            in.next();
+        }
+
         return in.nextInt();
     }
 
     public int slicesPerPizza() {
         System.out.print("How many slices per pizza? ");
+
+        // User can only enter number
+        while (!in.hasNextInt()) {
+            System.out.print("Invalid number of slices. Enter again: ");
+            in.next();
+        }
+
         return in.nextInt();
     }
 
     public String stringConcat(int numPeople, int numPizzas, int totalSlices, int pizzaPerPerson, int leftoverPizza) {
-        String stringOne = String.format("%d people with %d pizzas (%d slices)\n", numPeople, numPizzas, totalSlices);
-        String stringTwo = String.format("Each person gets %d pieces of pizza.\n", pizzaPerPerson);
-        String stringThree = String.format("There are %d leftover pieces.", leftoverPizza);
+
+        String pizzaInsert = numPizzas == 1 ? "pizza" : "pizzas";
+        String slicesInsert = totalSlices == 1 ? "slice" : "slices";
+        String piecesInsert = pizzaPerPerson == 1 ? "piece" : "pieces";
+        String peopleInsert = numPeople == 1 ? "person" : "people";
+
+        String stringOne = String.format("%d %s with %d %s (%d %s)\n", numPeople, peopleInsert, numPizzas, pizzaInsert,
+                totalSlices, slicesInsert);
+        String stringTwo = String.format("Each person gets %d %s of pizza.\n", pizzaPerPerson, piecesInsert);
+        String stringThree = leftoverPizza == 1 ? String.format("There is %d leftover piece.", leftoverPizza) :
+                String.format("There are %d leftover pieces.", leftoverPizza);
 
         return stringOne + stringTwo + stringThree;
     }
